bff49793184f1d6860d79a29d986e5cbdc260c8e
ikede-divine-keno <ikededivine@gmail.com>
2023-08-18 07:23:15 -0700

    3-user.py modified again and debugged

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -40,7 +40,8 @@ class User():
         if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
-            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
+        #solution 1: change ._ to .__
 
     def is_valid_password(self, pwd):
         """
@@ -54,7 +55,8 @@ class User():
             return False
         if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
+        #solution 2: change upper to lower to avoid mismatch
 
 
 if __name__ == '__main__':
@@ -84,9 +86,9 @@ if __name__ == '__main__':
     if user_2.password is not None:
         print("User.password should be None if setter to an integer")
 
-    """if not user_1.is_valid_password(u_pwd):
+    if not user_1.is_valid_password(u_pwd):
         print("is_valid_password should return True if it's the right \
-password")"""
+password")
 
     if user_1.is_valid_password("Fakepwd"):
         print("is_valid_password should return False if it's not the right \
